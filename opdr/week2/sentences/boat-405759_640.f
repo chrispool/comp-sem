39e9f091d7c2e624de9f0fbd2733bef3e3328725
System Administrator <root@MacBook-Pro-van-Chris.local>
2015-11-25 13:10:19 +0100

    added files for week2

---
+++ opdr/week2/sentences/boat-405759_640.f
@@ -0,0 +1,3 @@
+A car drives on the pond
+The heron is flying over the pond
+The boat is red
---
+++ opdr/week2/sentences/boy-214084_640.f
@@ -0,0 +1,3 @@
+The boy washes the boat
+The boy is driving a car
+The boy is washing the car with a hose
